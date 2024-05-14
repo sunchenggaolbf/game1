3587ffe318d50c2ee2a976cbc3d834303ff83efb
sunerbao <sunchenggao.lbf@gmail.com>
2024-05-14 21:32:08 +0900

    222

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ const App = () => {
 
     Matter.World.add(world, [ground, leftWall, rightWall]);
 
-    // 不断更新小球位置状态监听111
+    // 不断更新小球位置状态监听111222
     Matter.Events.on(engine, 'afterUpdate', () => {
       setBall(ball => {
         if (ball) {
