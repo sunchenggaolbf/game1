a929b2cc4fcdcf55beb901cbffa7a2c86e499e98
sunerbao <sunchenggao.lbf@gmail.com>
2024-05-14 22:34:34 +0900

    5555555555555555

--- App.js
+++ App.js
@@ -28,7 +28,7 @@ const App = () => {
       });
     });
 
-    // 碰撞检测监听111
+    // 碰撞检测监听55555555555555
     Matter.Events.on(engine, 'collisionStart', event => {
       event.pairs.forEach(pair => {
         const { bodyA, bodyB } = pair;
