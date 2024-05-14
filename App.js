aadfa4495b0c28641efac5a874a7f6ba46bd1888
sunerbao <sunchenggao.lbf@gmail.com>
2024-05-14 22:48:24 +0900

    Revert "6666"
    This reverts commit 310935faf5a6efab33c2f67e5aa1021527083587.

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ const App = () => {
 
     Matter.World.add(world, [ground, leftWall, rightWall]);
 
-    // 不断更新小球位置状态监听66666
+    // 不断更新小球位置状态监听111111111
     Matter.Events.on(engine, 'afterUpdate', () => {
       setBall(ball => {
         if (ball) {
